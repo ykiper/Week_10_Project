c6d48a2866dca2e9bd7ec2da497a5714e25bca37
Yossi kiper <ykipper1@gmail.com>
2025-12-24 11:56:30 +0200

    add the fastapi server and improvements to crud functions

--- app/data_interactor.py
+++ app/data_interactor.py
@@ -1,7 +1,9 @@
 import mysql.connector
 from contact import Contact
 
-def connect_to_db(query):
+
+#write all in class sqlService
+def connect_to_db():
     conn = mysql.connector.connect(
         host='127.0.0.1',
         user="root",
@@ -9,13 +11,7 @@ def connect_to_db(query):
         password="",
         database='contacts_db')
 
-    cursor = conn.cursor(buffered=True)
-
-    cursor.execute(query)
-
-    conn.commit()
-    conn.close()
-    return cursor.fetchall()
+    return conn
 
 
 def create_contact(first_name, last_name, phone_number):
@@ -23,30 +19,43 @@ def create_contact(first_name, last_name, phone_number):
     INSERT INTO contacts (first_name, last_name, phone_number)
     VALUES ("{first_name}", "{last_name}", "{phone_number}");
     """
+    conn = connect_to_db()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    id_num = cursor.lastrowid
+    conn.commit()
+    conn.close()
 
-    connect_to_db(query)
-
-    query = "select max(id) from contacts"
-    id_num = connect_to_db(query)[0][0]
-
-    return id_num
+    return {
+"message": "Contact created successfully",
+"id": id_num
+}
 
 
 def get_all_contacts():
+    conn = connect_to_db()
+    cursor = conn.cursor()
     query = "select * from contacts"
-    contacts = connect_to_db(query)
+    cursor.execute(query)
+    contacts = cursor.fetchall()
     contact_list = []
     for contact in contacts:
         con_object = Contact(*contact)
         contact_list.append(con_object.convert_to_dict())
+    conn.commit()
+    conn.close()
     return contact_list
 
-print(get_all_contacts())
 
-def update_contact():
+def update_contact(id):
     pass
 
 def delete_contact(id):
+    conn = connect_to_db()
+    cursor = conn.cursor()
     query = f"delete from contacts where id={id}"
-    connect_to_db(query)
+    cursor.execute(query)
+    conn.commit()
+    conn.close()
     return True
+
--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
+from data_interactor import create_contact, get_all_contacts, delete_contact
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+
+class Item(BaseModel):
+    first_name: str
+    last_name: str
+    phone_number: str
+
+
+
+app = FastAPI()
+
+@app.get("/contacts")
+def get_contacts():
+    return get_all_contacts()
+
+
+@app.post("/contacts")
+def create_contact_API(item: Item):
+    return create_contact(item.first_name, item.last_name, item.phone_number)
+
+
+@app.put("/contacts/{id}")
+def update_contact_API(it: int, item: Item)):
+    return
+
+
+@app.delete("/contacts/{id}")
+def delete_contact_API(id: int):
+    return delete_contact(id)
+
 
